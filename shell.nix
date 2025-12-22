d4d93c98f76abfe5b9303aadc4c2e18c6af9991e
tuturuu <zedddiezxc@gmail.com>
2025-12-22 03:41:07 +0100

    build: fix wayland crash problem
    include libGL libxkbcommon wayland
    to fix NixOS compatability issue with wayland;
    change shell.nix and move dev dependencies to
    devShell

--- shell.nix
+++ shell.nix
@@ -1,10 +1,29 @@
-{ pkgs ? import <nixpkgs> { } }:
-pkgs.mkShell {
-  nativeBuildInputs = with pkgs.buildPackages; [
-    rustc
-    cargo
-    rust-analyzer
-    clippy
-    rustfmt
-  ];
+{
+  pkgs ? import <nixpkgs> { },
+}:
+let
+  libPath =
+    with pkgs;
+    lib.makeLibraryPath [
+      libGL
+      libxkbcommon
+      wayland
+    ];
+in
+{
+  devShell =
+    with pkgs;
+    mkShell {
+      buildInputs = [
+        rustc
+        cargo
+        rust-analyzer
+        clippy
+        rustfmt
+      ];
+
+      RUST_LOG = "debug";
+      RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+      LD_LIBRARY_PATH = libPath;
+    };
 }
