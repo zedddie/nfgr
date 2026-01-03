58e259b420512f359b3d45a6fec616cf43e5c882
tuturuu <zedddiezxc@gmail.com>
2026-01-03 21:10:32 +0100

    feat: write precalc_roots fn

--- src/math/calc_buffer.rs
+++ src/math/calc_buffer.rs
@@ -46,16 +46,61 @@ pub fn calculate_buffer<F>(
         }
     }
 }
+// root expectant
+struct RootE {
+    root: Complex,
+    counter: u32,
+}
+pub fn precalc_root_colours<F>(
+    width: &usize,
+    height: &usize,
+    f: F,
+    math_plot_size: &MathPlot,
+) -> Vec<Complex>
+where
+    F: Fn(DComplex) -> DComplex + Clone,
+{
+    let mut root_vec = Vec::<RootE>::new();
+
+    let pq_step_x = (math_plot_size.x / *width as f64) * 10_f64;
+    let pq_step_y = (math_plot_size.y / *height as f64) * 10_f64;
+
+    for py in 0..*height / 10 {
+        for px in 0..*width / 10 {
+            let rl = -(math_plot_size.x / 2_f64) + (px as f64 + 0.5) * pq_step_x;
+            let im = math_plot_size.y / 2_f64 - (py as f64 + 0.5) * pq_step_y;
+
+            let conv_root = calc_root(Complex { rl, im }, &f, 300);
+            match root_vec.iter_mut().find(|root_e| {
+                (root_e.root.im - conv_root.0.im).abs() < 1e-4
+                    && (root_e.root.rl - conv_root.0.rl).abs() < 1e-4
+            }) {
+                None => root_vec.push(RootE {
+                    root: conv_root.0,
+                    counter: 1,
+                }),
+                root => root.unwrap().counter += 1,
+            }
+        }
+    }
+    let roots: Vec<Complex> = root_vec
+        .iter()
+        .filter(|e| e.counter >= 20)
+        .map(|r| r.root)
+        .collect();
+    roots
+}
 fn apply_coloring(z: Complex, iters: usize, max_iter: usize) -> u32 {
     if iters >= max_iter {
         return 0x000000;
     }
+    42
 
-    let angle = z.im.atan2(z.rl);
-    let hue = (angle + std::f64::consts::PI) / (2.0 * std::f64::consts::PI);
-    let intensity = 1.0 - (iters as f32 / max_iter as f32).powf(0.5);
-
-    hsv_to_rgb(hue as f32, 0.8, intensity)
+    // let angle = z.im.atan2(z.rl);
+    // let hue = (angle + std::f64::consts::PI) / (2.0 * std::f64::consts::PI);
+    // let intensity = 1.0 - (iters as f32 / max_iter as f32).powf(0.5);
+    //
+    // hsv_to_rgb(hue as f32, 0.8, intensity)
 }
 
 // h: 0.0-1.0, s: 0.0-1.0, v: 0.0-1.0
